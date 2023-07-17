8983e4fba5ec8ebbc489ca7c97bce8e48de8459a
Doniblaze <donaldbassey0@gmail.com>
2023-07-17 17:15:32 +0100

    nftdao

--- src/java/com/bartmint/users/NFTDAO.java
+++ src/java/com/bartmint/users/NFTDAO.java
@@ -1,6 +1,23 @@
 package com.bartmint.users;
 
+import com.bartmint.dbconfig.DBConfig;
+import javax.persistence.EntityManager;
+
 public class NFTDAO
 {
-
+    public static void registerNewUsers(NFT nft)
+    {
+        try(DBConfig DBconfig = new DBConfig())
+        {
+            EntityManager DBconfigem = DBconfig.getEntityManager();
+            DBconfigem.getTransaction().begin();
+            DBconfigem.persist(nft);
+            DBconfigem.getTransaction().commit();
+        }
+        catch(Exception DBconfig)
+        {
+            DBconfig.printStackTrace(System.err);
+            throw new RuntimeException(DBconfig);
+        }
+    }
 }
